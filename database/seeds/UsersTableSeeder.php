d616f45736b55cb37f19bfd9caeda5111ed63c45
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-04-26 17:52:17 +0200

    Add users table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use Carbon\Carbon;
+
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // A hash of "password".
+        $password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+
+        // Create a superadmin user
+        $user = new User;
+        $user->name = 'Superadmin bruker';
+        $user->email = 'superadmin@example.com';
+        $user->password = $password;
+        $user->email_verified_at = Carbon::now();
+        $user->remember_token = Str::random(10);
+        $user->save();
+
+        $user->assignRole('superadmin');
+        $user->assignRole('admin');
+        $user->assignRole('user');
+        $user->save();
+        
+        // Create an admin user
+        $user = new User;
+        $user->name = 'Admin bruker';
+        $user->email = 'admin@example.com';
+        $user->password = $password;
+        $user->email_verified_at = Carbon::now();
+        $user->remember_token = Str::random(10);
+        $user->save();
+        
+        $user->assignRole('admin');
+        $user->assignRole('user');
+        $user->save();
+        
+        // Create a regular user
+        $user = new User;
+        $user->name = 'Vanlig bruker';
+        $user->email = 'user@example.com';
+        $user->password = $password;
+        $user->email_verified_at = Carbon::now();
+        $user->remember_token = Str::random(10);
+        $user->save();
+
+        $user->assignRole('user');
+        $user->save();
+    }
+}
