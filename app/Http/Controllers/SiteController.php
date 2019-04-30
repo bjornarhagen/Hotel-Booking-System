ffe6f7e9ff1ea0a22f4628c1c5f8dc2acc168e8c
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-04-30 23:01:56 +0200

    Make frontpage redirect to Havnehotellet i Halden

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Hotel;
+
 class SiteController extends Controller
 {
     /**
@@ -11,6 +13,9 @@ class SiteController extends Controller
      */
     public function index()
     {
-        return view('site.index');
+        // return view('site.index');
+
+        $hotel = Hotel::where('slug', 'havnehotellet-i-halden')->first();
+        return redirect()->route('hotel.show', $hotel->slug);
     }
 }
