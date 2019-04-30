97be214f235383ded45c9c666f7fe40ce352996a
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-04-30 23:49:31 +0200

    Update seeds to create Havnehotellet i Halden

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Hotel;
+use App\Room;
+use App\RoomType;
+use App\HotelUser;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,8 +18,67 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesTableSeeder::class);
 
+        // Make the hotel
+        $hotel = new Hotel;
+        $hotel->name = 'Havnehotellet i Halden';
+        $hotel->slug = Str::slug($hotel->name);
+        $hotel->brand_color_primary = '#AE9731';
+        $hotel->brand_color_accent = '#000000';
+        $hotel->website = 'https://havnehotellet-i-halden.no';
+        $hotel->contact_email = 'post@hhih.no';
+        $hotel->contact_phone = '6922334455';
+        $hotel->address_street = 'Mathias Bjørns gate';
+        $hotel->address_city = 'Halden';
+        $hotel->address_zip = '1771';
+        $hotel->address_lat = '59.1192366';
+        $hotel->address_lon = '11.3751791';
+        $hotel->parking_spots = 14;
+        $hotel->price_parking_spot = 0;
+        $hotel->price_meal_breakfast = 0;
+        $hotel->price_meal_lunch = 100;
+        $hotel->price_meal_dinner = 250;
+        $hotel->save();
+
+        $room_types = [
+            'enkeltrom' => [
+                'price' => 990,
+                'count' => 10
+            ],
+            'dobbeltrom' => [
+                'price' => 1290,
+                'count' => 8
+            ],
+            'suite' => [
+                'price' => 1990,
+                'count' => 2
+            ],
+            'bryllupssuite' => [
+                'price' => 2490,
+                'count' => 1
+            ]
+        ];
+
+        // Make room types
+        foreach ($room_types as $key => $info) {
+            $room_type = new RoomType;
+            $room_type->hotel_id = $hotel->id;
+            $room_type->name = ucfirst($key);
+            $room_type->slug = Str::slug($room_type->name);
+            $room_type->price = $info['price'];
+            $room_type->save();
+
+            for ($i=0; $i < $info['count']; $i++) { 
+                $room = new Room;
+                $room->room_type_id = $room_type->id;
+                $room->group = $room_type->name[0];
+                $room->number = $i+1;
+                $room->save();
+            }
+        }
+
+        $this->call(UsersTableSeeder::class);
+
         if (app()->environment('local')) {
-            $this->call(UsersTableSeeder::class);
             $this->call(HotelsTableSeeder::class);
             $this->call(FacilitiesTableSeeder::class);
             $this->call(RoomTypesTableSeeder::class);
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\User;
+use App\Role;
+use App\Hotel;
+use App\HotelUser;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
@@ -15,6 +18,9 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $hotel = Hotel::where('slug', 'havnehotellet-i-halden')->first();
+        $role = Role::where('slug', 'hotel_manager')->first();
+
         $user = new User;
         $user->name_first = 'Example';
         $user->name_last = 'User';
@@ -24,6 +30,29 @@ class UsersTableSeeder extends Seeder
         $user->remember_token = Str::random(10);
         $user->save();
 
-        factory(User::class, 50)->create();
+        $hotel_user = new HotelUser;
+        $hotel_user->user_id = $user->id;
+        $hotel_user->hotel_id = $hotel->id;
+        $hotel_user->role_id = $role->id;
+        $hotel_user->save();
+
+        $user = new User;
+        $user->name_first = 'Ola';
+        $user->name_last = 'Nordmann';
+        $user->email = 'sensor';
+        $user->password = Hash::make('sensor');
+        $user->email_verified_at = Carbon::now();
+        $user->remember_token = Str::random(10);
+        $user->save();
+
+        $hotel_user = new HotelUser;
+        $hotel_user->user_id = $user->id;
+        $hotel_user->hotel_id = $hotel->id;
+        $hotel_user->role_id = $role->id;
+        $hotel_user->save();
+
+        if (app()->environment('local')) {
+            factory(User::class, 50)->create();
+        }
     }
 }
