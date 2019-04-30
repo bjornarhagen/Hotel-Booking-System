1c3e1c34fab26dc07e838bc6ed7dcf6df41ebb3b
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-05-01 00:08:44 +0200

    Add Blade directive for including svg icons into view. Add fallback icon. Add icons resource folder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,6 +31,7 @@ class AppServiceProvider extends ServiceProvider
         $this->bootSetTimeLocale();
         $this->bootBladeHotelRole();
         $this->bootBladeHotelGuest();
+        $this->bootBladeIcon();
     }
     
     private function bootSetTimeLocale()
@@ -58,6 +59,40 @@ class AppServiceProvider extends ServiceProvider
         Blade::if('hotel_guest', function () {
             $user = Auth::user();
             return $user->isOnlyGuest();
+        });
+    }
+
+    private function bootBladeIcon()
+    {
+        Blade::directive('icon', function ($arguments) {
+            // Accept multiple arguments into the directive
+            list($path, $class) = array_pad(explode(',', trim($arguments, "() ")), 2, '');
+            $path = trim($path, "' ");
+            $path = public_path('icons/' . $path . '.svg');
+            $class = trim($class, "' ");
+
+            // Set a fallback icon
+            if (!file_exists($path)) {
+                $path = public_path('icons/icon-missing.svg');
+            }
+
+            // Create the dom document
+            $svg = new \DOMDocument();
+            $svg->load($path);
+            $svg->documentElement->setAttribute("class", trim('icon ' . $class));
+            $svg->documentElement->setAttribute('aria-hidden', 'true');
+
+            // Remove the title
+            $svg_title = $svg->getElementsByTagName("title")->item(0);
+            if ($svg_title != null) {
+                $svg_head = $svg_title->parentNode;
+                $svg_head->removeChild($svg_title);
+            }
+
+            $output = $svg->saveXML($svg->documentElement);
+            $output = preg_replace('/\s+/', ' ', $output); // Remove line breaks and duplicate whitespace
+
+            return $output;
         });
     }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ const mix = require("laravel-mix");
 mix.js("resources/js/app.js", "public/js").version();
 mix.js("resources/js/user.js", "public/js").version();
 mix.sass("resources/sass/app.scss", "public/css").version();
+mix.copy("resources/icons", "public/icons");
 
 mix.browserSync({
     proxy: "localhost:8000",
