0cfc45cd903b93b1e295456dfe4bb50cb6683d76
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-04-30 19:01:50 +0200

    Fix composite key issues

--- app/BookingUser.php
+++ app/BookingUser.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasCompositeKey;
 
 class BookingUser extends Model
 {
+    use HasCompositeKey;
+
+    protected $primaryKey = ['booking_id', 'user_id'];
     public $timestamps = false;
 }
