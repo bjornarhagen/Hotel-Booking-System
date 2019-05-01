62e3f3e3dd8c7386a4eba8251165c26b0e36a8c5
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-05-01 17:41:23 +0200

    Add chevrons to the booking step nav

--- resources/views/booking/nav.blade.php
+++ resources/views/booking/nav.blade.php
@@ -4,23 +4,25 @@
     >
         {{ __('Step') }} 1
     </a>
+    @icon('chevron-right')
 
     <a class="nav-step{{ $step === 2 ? ' nav-step-active' : null }}"
         href="{{ route('hotel.booking.step-2', $hotel->slug) }}"
     >
         {{ __('Step') }} 2
     </a>
+    @icon('chevron-right')
 
     <a class="nav-step{{ $step === 3 ? ' nav-step-active' : null }}"
         href="{{ route('hotel.booking.step-3', $hotel->slug) }}"
     >
         {{ __('Step') }} 3
     </a>
+    @icon('chevron-right')
 
     <a class="nav-step{{ $step === 4 ? ' nav-step-active' : null }}"
         href="{{ route('hotel.booking.step-4', $hotel->slug) }}"
     >
         {{ __('Step') }} 4
     </a>
-
 </nav>
