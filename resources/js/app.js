67c35e20931b40b7dc4538d51fb92bf3617368ef
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-05-01 21:30:55 +0200

    Style booking step 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,7 @@ window.addEventListener("load", ready);
 
 function ready() {
     setupDropdowns();
+    setupExpands();
 }
 
 function setupDropdowns() {
@@ -20,6 +21,24 @@ function setupDropdowns() {
             const trigger = dropdown.querySelector(".dropdown-trigger");
             trigger.addEventListener("click", () => {
                 dropdown.classList.toggle("open");
+            });
+        });
+    }
+}
+
+function setupExpands() {
+    const expands = document.querySelectorAll(".expand");
+
+    if (expands.length > 0) {
+        expands.forEach(expand => {
+            const trigger = expand.querySelector(".expand-trigger");
+
+            if (trigger == null) {
+                return;
+            }
+
+            trigger.addEventListener("click", () => {
+                expand.classList.toggle("open");
             });
         });
     }
--- resources/views/booking/show-step-2.blade.php
+++ resources/views/booking/show-step-2.blade.php
@@ -11,43 +11,76 @@
             <p>{{ __('Check in') }}: {{ $check_in_date->format('d. F Y') }}</p>
             <p>{{ __('Check out') }}: {{ $check_out_date->format('d. F Y') }}</p>
 
-            <button type="submit">{{ __('Continue') }}</button>
+            @php
+                $nights = $check_in_date->diffInDays($check_out_date);
+            @endphp
+
+            <button type="submit">
+                <span>{{ __('Continue') }}</span>
+                @icon('chevron-right')
+            </button>
         </header>
         @foreach ($room_types as $room_type)
             @php
                 $availableRooms = $room_type->availableRooms($check_in_date, $check_out_date);
                 $unavailableRooms = $room_type->unavailableRooms($check_in_date, $check_out_date);
             @endphp
-            <hr>
-            <section>
-                <h3>{{ $room_type->name }}</h3>
+            <section class="room_type expand">
+                <h2>{{ $room_type->name }}</h2>
 
                 @if ($availableRooms->count() === 0)
                     <p>Ingen ledige rom.</p>
                 @else
                     @foreach ($availableRooms as $room)
-                        <article class="available{{ $loop->first ? 'first' : null }}">
-                            <h3>
-                                <label for="room-checkbox-{{ $room->id }}">{{ $room->name }}</label>
-                                <input id="room-checkbox-{{ $room->id }}"
+                        @php
+                            $room_classes = 'room available';
+                            
+                            if ($loop->index == 0) {
+                                $room_classes .= ' first';
+                            }
+
+                            if (in_array($room->id, old('rooms', session('booking-rooms', [])))) {
+                                $checked =  'checked';
+                            } else {
+                                $checked = null;
+                            }
+
+                        @endphp
+                        <article class="{{ $room_classes }}">
+                            <div class="image" style="background-image: url('{{ asset('images/' . $hotel->slug . '/' . $room->room_type->slug . '.jpg') }}')"></div>
+                            <h3 class="name">{{ $room->name }}</h3>
+                            <p class="description">{{ $room->room_type->description }}</p>
+                            <input id="room-checkbox-{{ $room->id }}"
                                 name="rooms[]"
                                 type="checkbox"
                                 value="{{ $room->id }}"
-                                {{ in_array($room->id, old('rooms', session('booking-rooms', []))) ? 'checked' : null }}>
-                            </h3>
-                            <p>{{ __('Price') }}: {{ $room->price . ',-' }}</p>
-                            <p>Available</p>
-
-                            @if ($loop->first)
-                                <button type="button">Se flere ({{ $loop->remaining }})</button>
-                            @endif
+                                {{ $checked }}
+                            >
+                            <label class="select" for="room-checkbox-{{ $room->id }}">
+                                <p class="price">{{ __('Price') }}: {{ $room->price . ',-' }}</p>
+                                @if ($nights > 1)
+                                    <p class="price-nights">{{ __('Price for :nights nights', ['nights' => $nights]) }}: {{ ($room->price * $nights) . ',-' }}</p>
+                                @endif
+                                <p class="button button-primary">
+                                    @icon('checkbox-blank', 'not-selected')
+                                    @icon('checkbox-checked', 'selected')
+                                    <span>{{ __('Select room') }}</span>
+                                </p>
+                            </label>
                         </article>
+                        @if ($loop->first && $loop->remaining > 0)
+                            <div class="see-more-button">
+                                <button type="button" class="button-blank expand-trigger">
+                                    <span>{{ __('See more') }} ({{ $loop->remaining }})</span>
+                                    <span>{{ __('See less') }}</span>
+                                </button>
+                            </div>
+                        @endif
                     @endforeach
                 @endif
                 @if ($unavailableRooms->count() !== 0)
-                    <hr>
                     @foreach ($unavailableRooms as $room)
-                        <article class="busy">
+                        <article class="room busy">
                             <h3>{{ $room->id }}</h3>
                             <p>Busy</p>
                         </article>
@@ -55,5 +88,11 @@
                 @endif
             </section>
         @endforeach
+
+        <button class="submit" type="submit">
+            <span>{{ __('Continue') }}</span>
+            @icon('chevron-right')
+        </button>
     </form>
+    <link href="{{ asset('css/booking-step-2.css') }}" rel="stylesheet">
 @endsection
